9ba7a42a9235a57e2fbed0273f2d2124729b86c7
jstaffans <johannes.staffans@gmail.com>
2016-04-15 14:32:44 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kinesis-logger "0.1.1"
+(defproject kinesis-logger "0.1.2"
   :description "Simple logger for Kinesis events, mainly for debugging purposes"
   :url "https://github.com/komoot/kinesis-logger"
   :license {:name "Eclipse Public License"
